5a9164ff7c28eea563f4b2c62a7c70e3a0081a18
Lillie Kusel <lilliesmacbook@Lillies-MacBook-Air.local>
2022-02-04 17:35:24 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	    int fallCost = 500;
+        int winterCost = 700;
+        int springCost = 200;
+        int summerCost = 400;
+        int totalCost = 0;
+
+        totalCost = fallCost + winterCost + springCost + summerCost;
+        System.out.println(" The total yearly cost is " + totalCost);
+    }
+}
